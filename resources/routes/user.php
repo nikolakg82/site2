664ff39387cb4e83cc901133f81f1cd78d485182
Nikola <dzoni82.kg@gmail.com>
2018-05-13 19:53:21 +0200

    Add routes for user

--- resources/routes/user.php
+++ resources/routes/user.php
@@ -7,3 +7,10 @@ Router::addRoute(
     '/login.{view|' . FM_REQUIRED . '|values:' . FM_JSON . '}',
     'login',
     FM_POST);
+
+Router::addRoute(
+    CMS_C_USER,
+    '/logout.{view|' . FM_REQUIRED . '|values:' . FM_JSON . '}',
+    'logout',
+    FM_GET,
+    CMS_USER | CMS_ADMIN);
